fc2c6437b8c361b274cf8bc9c7f683bbc90048d4
NatalyaVoronova <loginova2024.1@gmail.com>
2025-04-06 11:06:00 +0500

    Simple Object Access Protocol (SOAP)(L 9.5).

--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -16,3 +16,10 @@ def test_signup_new_account(app):
     assert app.session.is_logged_in_as(username)
     app.session.logout()
 
+def test_signup_new_account_soap(app):
+    username = random_username("user_", 10)
+    email = username + "@localhost"
+    password = "test"
+    app.james.ensure_user_exist(username, password)
+    app.signup.new_user(username, email, password)
+    assert app.soap.can_login(username, password)
